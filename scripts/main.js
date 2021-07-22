b796641f7d55d7d94a86ca1be95d554dcf36cf7a
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-22 10:36:16 +0200

    Add environment variable

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
+const ApiKey = require("./api_key");
+
 class MovieFinder {
     constructor() {}
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    console.log(ApiKey);
     const meteo = new MovieFinder();
 });
