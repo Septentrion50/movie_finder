28e277a505242760d8977bc3caa968c49187457d
Septentrion50 <erwan56lecorre@gmail.com>
2021-07-22 11:26:33 +0200

    Add api_key object

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
-const ApiKey = require("./api_key");
-
 class MovieFinder {
     constructor() {}
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(ApiKey);
     const meteo = new MovieFinder();
 });
